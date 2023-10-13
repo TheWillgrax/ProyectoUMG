5e80c1bf806f1c7abed7d409ebfd1bd5d754d7a5
TheWillgrax <145234634+TheWillgrax@users.noreply.github.com>
2023-10-13 00:07:26 -0600

    Mejoras Proyecto

--- src/main/java/me/parzibyte/sistemaventasspringboot/UsuarioRepository.java
+++ src/main/java/me/parzibyte/sistemaventasspringboot/UsuarioRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    // Puedes agregar métodos personalizados si es necesario
 }

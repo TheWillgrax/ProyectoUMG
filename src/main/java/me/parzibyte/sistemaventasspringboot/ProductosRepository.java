2ba762dfaa71117cffc7bd20741513b9021e932e
TheWillgrax <145234634+TheWillgrax@users.noreply.github.com>
2023-10-15 01:57:21 -0600

    Mejoras en el Login

--- src/main/java/me/parzibyte/sistemaventasspringboot/ProductosRepository.java
+++ src/main/java/me/parzibyte/sistemaventasspringboot/ProductosRepository.java
@@ -3,6 +3,5 @@ package me.parzibyte.sistemaventasspringboot;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductosRepository extends CrudRepository<Producto, Integer> {
-
     Producto findFirstByCodigo(String codigo);
 }

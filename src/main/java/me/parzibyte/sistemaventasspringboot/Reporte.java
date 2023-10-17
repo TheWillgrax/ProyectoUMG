9e207921bdea148949de9b1586a3e6d10face507
TheWillgrax <145234634+TheWillgrax@users.noreply.github.com>
2023-10-16 23:52:54 -0600

    Agregando Librerias para el Reporte

---
+++ src/main/java/me/parzibyte/sistemaventasspringboot/Reporte.java
@@ -0,0 +1,10 @@
+package me.parzibyte.sistemaventasspringboot;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class Reporte {
+
+}
